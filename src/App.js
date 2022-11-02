2ab5dee1b4ac5813b2549fc7a6edba24ad453eb9
Usmanov <roma.maksato@gmail.com>
2022-11-02 20:03:26 +0600

    new

--- src/App.js
+++ src/App.js
@@ -22,62 +22,80 @@ function App() {
               </div>
             </div>
           <div className="main">
-            <h2 className="service">Наши услуги</h2>
+            <h2 className="service">Прайс:</h2>
             <p className="circle">Барбер</p>
             <p className="circle1">Топ барбер</p>
             <p className="circle2">Вип барбер</p>
             <ul className="ulBlock">
               <li className="UlBlockPrice">
-                <p>Мужская стрижка:</p>
+                <p className="name">Мужская стрижка :</p>
                   <p>400c\</p>
                 <p>500с\</p>
                 <p>800с</p>
               </li>
               <li className="UlBlockPrice">
-                <p>Детская стрижка:</p>
+                <p className="name">Детская стрижка:</p>
                 <p>350c\</p>
                 <p>400с\</p>
                 <p>500с</p>
               </li><li className="UlBlockPrice">
-              <p>Бритье головы:</p>
+              <p className="name">Бритье головы:</p>
               <p>350c\</p>
               <p>400с\</p>
               <p>600с</p>
             </li><li className="UlBlockPrice">
-              <p>Стрижка бороды:</p>
+              <p className="name">Стрижка бороды:</p>
               <p>350c\</p>
               <p>400с\</p>
               <p>600с</p>
             </li>
               <li className="UlBlockPrice">
-              <p>Бритье бороды:</p>
+              <p className="name">Бритье бороды:</p>
               <p>350c\</p>
               <p>400с\</p>
               <p>600с</p>
             </li>
               <li className="UlBlockPrice">
-                <p>Укладка волс:</p>
+                <p className="name">Укладка волс:</p>
                 <p>200c\</p>
                 <p>250с\</p>
                 <p>300с</p>
               </li>
               <li className="UlBlockPrice">
-                <p>Камуфляж волос:</p>
+                <p className="name">Камуфляж волос:</p>
                 <p>350c\</p>
                 <p>400с\</p>
                 <p>500с</p>
               </li>
               <li className="UlBlockPrice">
-                <p>Камуфляж бороды:</p>
+                <p className="name">Камуфляж бороды:</p>
                 <p>350c\</p>
                 <p>350с\</p>
                 <p>500с</p>
               </li>
               <li className="UlBlockPrice">
-                <p>Маска лица 3 в 1:</p>
+                <p className="name">Маска лица 3 в 1:</p>
                 <p>500c\</p>
                 <p>500с\</p>
                 <p>600с</p>
+              </li>
+              <li className="UlBlockPrice">
+                <p className="name">Массаж головы:</p>
+                <p>100c\</p>
+                <p>100с\</p>
+                <p>200с</p>
+              </li>
+              <li className="UlBlockPrice">
+                <p className="name">Восковая дипиляция:</p>
+                <p>100c\</p>
+                <p>150с\</p>
+                <p>200с</p>
+              </li>
+              <li className="UlBlockPrice">
+                <p className="name">Кудри + Срижка:</p>
+                <p>1500c\</p>
+                <p>1700с\</p>
+                <p>2000с</p>
               </li>
             </ul>
           </div>
