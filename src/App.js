99afec584312b6302aec4a84447ddab35538eddb
Usmanov <roma.maksato@gmail.com>
2022-11-02 16:55:36 +0600

    cheking logo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import logo from '../src/assets/img/logo.png'
 
 function App() {
   return (
     <div className="App">
       <>
         <div className="container">
-          <div className="header">
+          <div className="logo">
+            <img src={logo} alt=""/>
           </div>
         </div>
       </>
