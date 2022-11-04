0c5787613d772d84344de925d1910857b5b857f0
Usmanov <roma.maksato@gmail.com>
2022-11-04 23:03:41 +0600

    new

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import logo from '../src/assets/img/300_s (1).pdf (1).png'
 function App() {
   return (
     <div className="App">
-      <div
-        className="container w-full h-auto overflow-scroll block h-screen bg-gradient-to-r from-blue-100 via-purple-100">
+      <div className="container w-full h-auto overflow-scroll block h-screen bg-gradient-to-r from-blue-100 via-purple-100">
         <div className="background">
           <section className="header">
             <div className="headerBox">
@@ -29,8 +28,101 @@ function App() {
           </section>
           <section className="main">
             <h1 className="titlePrice">Прайс</h1>
-            <h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, adipisci architecto aspernatur, aut dolore esse est fugiat maxime mollitia nostrum praesentium quibusdam quis rerum sed soluta? Atque consequuntur enim est eveniet expedita fugiat id, ipsam, ipsum laudantium neque nisi nulla, odit placeat possimus quia reiciendis repellat reprehenderit sed similique tempora vero vitae voluptatibus? Atque consequatur debitis delectus, dolorum eos expedita in, labore magnam maiores nemo perspiciatis, porro quaerat quia quos repellendus ullam vel veniam. Ab culpa deleniti dicta eos nam provident, quod repellat! A atque, commodi consequatur doloremque ducimus eligendi esse ipsa ipsum magnam modi nihil numquam quas rem velit.</h2>
+            <div className="barber1">Барбер</div>
+            <div className="barber2">Топ барбер</div>
+            <div className="barber3">Вип барбер</div>
+            <ul className="title">
+              <li className="liPrice">
+              <p className="liPriceTitle">Мужская стрижка ...</p>
+                <div className="div">
+                  <span className="liPricePrice">400c |</span>
+                  <span className="liPricePrice">500c |</span>
+                  <span className="liPricePrice">800c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Детская стрижка ...</p>
+                <div className="div">
+                  <span className="liPricePrice">350c |</span>
+                  <span className="liPricePrice">400c |</span>
+                  <span className="liPricePrice">500c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Бритье головы ...</p>
+                <div className="div">
+                  <span className="liPricePrice">350c |</span>
+                  <span className="liPricePrice">400c |</span>
+                  <span className="liPricePrice">600c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Стрижка бороды ...</p>
+                <div className="div">
+                  <span className="liPricePrice">350c |</span>
+                  <span className="liPricePrice">400c |</span>
+                  <span className="liPricePrice">600c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Бритье бороды ...</p>
+                <div className="div">
+                  <span className="liPricePrice">350c |</span>
+                  <span className="liPricePrice">400c |</span>
+                  <span className="liPricePrice">600c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Укладка волос ...</p>
+                <div className="div">
+                  <span className="liPricePrice">200c |</span>
+                  <span className="liPricePrice">250c |</span>
+                  <span className="liPricePrice">300c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Камуфляж волос ...</p>
+                <div className="div">
+                  <span className="liPricePrice">350c |</span>
+                  <span className="liPricePrice">400c |</span>
+                  <span className="liPricePrice">500c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Маска лица 3 в 1 ...</p>
+                <div className="div">
+                  <span className="liPricePrice">500c |</span>
+                  <span className="liPricePrice">500c |</span>
+                  <span className="liPricePrice">600c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Массаж головы ...</p>
+                <div className="div">
+                  <span className="liPricePrice">100c |</span>
+                  <span className="liPricePrice">100c |</span>
+                  <span className="liPricePrice">200c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Восковая Дипеляция ...</p>
+                <div className="div">
+                  <span className="liPricePrice">100c |</span>
+                  <span className="liPricePrice">150c |</span>
+                  <span className="liPricePrice">200c</span>
+                </div>
+              </li>
+              <li className="liPrice">
+                <p className="liPriceTitle">Кудри+Стрижка ...</p>
+                <div className="div">
+                  <span className="liPricePrice">1500c |</span>
+                  <span className="liPricePrice">1700c |</span>
+                  <span className="liPricePrice">2000c</span>
+                </div>
+              </li>
+            </ul>
           </section>
+
         </div>
       </div>
     </div>
