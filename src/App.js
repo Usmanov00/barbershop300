67ada0fb11745b6900beb955db21b86a0f812e44
Usmanov <roma.maksato@gmail.com>
2022-11-04 21:36:05 +0600

    mew

--- src/App.js
+++ src/App.js
@@ -4,27 +4,30 @@ import logo from '../src/assets/img/300_s (1).pdf (1).png'
 function App() {
   return (
     <div className="App">
-      <div className="container w-full h-auto overflow-scroll block h-screen bg-gradient-to-r from-blue-100 via-purple-100">
-       <section className="header">
-         <div className="headerBox">
-             <div className="container header-container">
-               <nav className="menu">
-                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                 <a href="#" className="menu-link">О нас</a>
-                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                 <a href="#" className="menu-link">Услуги</a>
-                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                 <a href="#" className="menu-link">Цены</a>
-                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-                 <a href="#" className="menu-link">Мастера</a>
-                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-               </nav>
-             </div>
-           <div className="img">
-             <img src={logo} alt=""/>
-           </div>
-         </div>
-       </section>
+      <div
+        className="container w-full h-auto overflow-scroll block h-screen bg-gradient-to-r from-blue-100 via-purple-100">
+        <div className="background">
+          <section className="header">
+            <div className="headerBox">
+              <div className="container header-container">
+                <nav className="menu">
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a href="#" className="menu-link">О нас</a>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a href="#" className="menu-link">Услуги</a>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a href="#" className="menu-link">Цены</a>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                  <a href="#" className="menu-link">Мастера</a>
+                  {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                </nav>
+              </div>
+              <div className="img">
+                <img src={logo} alt=""/>
+              </div>
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   );
